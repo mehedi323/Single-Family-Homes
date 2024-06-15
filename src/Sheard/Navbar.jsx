c8eb990f1afd21297519f5763517024bdf477ca4
Mehedi Hasan <dewanmehedi323@gmail.com>
2024-06-15 10:01:35 +0600

    root router navbar home banner components addad

---
+++ src/Sheard/Navbar.jsx
@@ -0,0 +1,41 @@
+import { NavLink } from "react-router-dom";
+import navImg from '../assets/singleHome-removebg-preview.png'
+
+
+const Navbar = () => {
+    const navLinks = <>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/about'>About Us</NavLink></li>
+        <li><NavLink to='/contact'>Contact</NavLink></li>
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box font-bold w-52">
+                        {navLinks}
+                    </ul>
+                </div>
+                <div className="flex items-center gap-5">
+                    <span>
+                        <img className="w-[80px]" src={navImg} alt="" />
+                    </span>
+                    <a className=" lg:text-4xl font-bold">Dream <span className="text-red-500">Home</span></a>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal font-bold px-1">
+                    {navLinks}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn  font-bold">Login</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
